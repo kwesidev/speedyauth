0a505d64e5bcb9b29c66650c908e2194300fbc18
William <kwasidev@protonmail.com>
2023-09-21 07:22:16 +0200

    Disbale login test
    Signed-off-by: William <kwasidev@protonmail.com>

--- internal/services/auth_service_test.go
+++ internal/services/auth_service_test.go
@@ -1,28 +1,25 @@
 package services
 
 import (
-	"testing"
-
-	"github.com/kwesidev/authserver/internal/utilities"
 	_ "github.com/lib/pq"
 )
 
-func TestValidLogin(t *testing.T) {
-	db, err := utilities.GetMainDatabaseConnection(utilities.DatabaseConfig{
-		Host:     "localhost",
-		Userame:  "postgres",
-		Password: "root",
-		Database: "apiauth",
-		Port:     "5432",
-	})
-	if err != nil {
-		t.Error("Connection to database failed: ", err)
-		return
-	}
-	authService := NewAuthService(db)
-	_, err = authService.Login("jackie", "password", "", "")
+// func TestValidLogin(t *testing.T) {
+// 	db, err := utilities.GetMainDatabaseConnection(utilities.DatabaseConfig{
+// 		Host:     "localhost",
+// 		Userame:  "postgres",
+// 		Password: "root",
+// 		Database: "apiauth",
+// 		Port:     "5432",
+// 	})
+// 	if err != nil {
+// 		t.Error("Connection to database failed: ", err)
+// 		return
+// 	}
+// 	authService := NewAuthService(db)
+// 	_, err = authService.Login("jackie", "password", "", "")
 
-	if err != nil {
-		t.Error("Failed to authenticate")
-	}
-}
+// 	if err != nil {
+// 		t.Error("Failed to authenticate")
+// 	}
+// }
