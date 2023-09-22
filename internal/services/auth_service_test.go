a58138976b7ec6f0b1e9e72cf45d971701f20b46
William <kwasidev@protonmail.com>
2023-09-22 21:54:42 +0200

    Checking if integration test work
    Signed-off-by: William <kwasidev@protonmail.com>

--- internal/services/auth_service_test.go
+++ internal/services/auth_service_test.go
@@ -1,25 +1,51 @@
 package services
 
 import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/kwesidev/authserver/internal/models"
+	"github.com/kwesidev/authserver/internal/utilities"
 	_ "github.com/lib/pq"
 )
 
-// func TestValidLogin(t *testing.T) {
-// 	db, err := utilities.GetMainDatabaseConnection(utilities.DatabaseConfig{
-// 		Host:     "localhost",
-// 		Userame:  "postgres",
-// 		Password: "root",
-// 		Database: "apiauth",
-// 		Port:     "5432",
-// 	})
-// 	if err != nil {
-// 		t.Error("Connection to database failed: ", err)
-// 		return
-// 	}
-// 	authService := NewAuthService(db)
-// 	_, err = authService.Login("jackie", "password", "", "")
+var db *sql.DB
 
-// 	if err != nil {
-// 		t.Error("Failed to authenticate")
-// 	}
-// }
+func setUp() {
+	var err error
+	db, err = utilities.GetMainDatabaseConnection(utilities.DatabaseConfig{
+		Host:     os.Getenv("PG_HOST"),
+		Username: os.Getenv("PG_USER"),
+		Password: os.Getenv("PG_PASSWORD"),
+		Database: os.Getenv("PG_DB"),
+		Port:     os.Getenv("PG_PORT"),
+	})
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+		return
+	}
+	userService := NewUserService(db)
+	_, err = userService.Register(models.UserRegistrationRequest{
+		Username:     "john.doe",
+		Password:     "password_2030333",
+		EmailAddress: "johndoe@localhost",
+		FirstName:    "john",
+		LastName:     "doe",
+		CellNumber:   "0731482947",
+	})
+
+}
+func TestMain(m *testing.M) {
+	setUp()
+	code := m.Run()
+	os.Exit(code)
+}
+func TestLogin(t *testing.T) {
+	authService := NewAuthService(db)
+	_, err := authService.Login("john.doe", "password_2030333", "", "")
+	if err != nil {
+		t.Error("Failed to authenticate")
+	}
+}
