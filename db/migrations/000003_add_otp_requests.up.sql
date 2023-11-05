21957a19db91f93a0dd84d64ebb082d29fd86b81
William <kwasidev@protonmail.com>
2023-11-05 07:16:22 +0200

    Add new otp request table

---
+++ db/migrations/000003_add_otp_requests.up.sql
@@ -0,0 +1,13 @@
+BEGIN;
+CREATE TABLE otp_requests (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) NOT NULL,
+    ip_address VARCHAR ,
+    user_agent VARCHAR ,
+    token VARCHAR UNIQUE NOT NULL,
+    code VARCHAR(6) NOT NULL,
+    expiry_time TIMESTAMP NOT NULL,
+    send_method enum_send_method NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+);
+COMMIT;
